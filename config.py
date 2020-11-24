aee287eeaeda7b43b79d8d25e2be6fc5eb0dd2fb
L0Lanbo <llb1212@hotmail.com>
2020-11-24 17:21:38 +0800

    更新到‘错误处理’部分

--- config.py
+++ config.py
@@ -6,3 +6,11 @@ class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'this_is_key'
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+	#发送错误信息的邮件设置
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	ADMINS = ['lilanbo@aisino.com']
