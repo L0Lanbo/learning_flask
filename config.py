aea7784f4790e7b6127c3b1cac7f7256f6cf5fc5
L0Lanbo <llb1212@hotmail.com>
2020-11-26 13:44:10 +0800

    commit

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ class Config(object):
 	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	ADMINS = ['lilanbo@aisino.com']
+	ADMINS = ['skfp_test@126.com']
